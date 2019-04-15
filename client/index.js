531c8557118e72a36a1438028b9a4c666a2d01b7
Dion Dirza <dion.dirza@gmail.com>
2019-04-15 13:40:05 +0800

    Add window.onerror

--- client/index.js
+++ client/index.js
@@ -4,6 +4,13 @@ import loadPolyfills from './polyfills';
  * Using webpackMode: "eager" will cause this dynamic import to be part of the main bundle,
  * not creating its own chunk
  */
+
+window.onerror = (message, url, line, col, error) => {
+  console.error(error.stack);
+  // TO-DO: create global function to show error on screen
+  // global.showErrorScreen(message);
+};
+
 (async () => {
   await loadPolyfills();
   import(/* webpackMode: "eager" */ './bootstrap.js');
