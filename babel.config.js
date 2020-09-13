34968ee44eaab885b00bc40158e3fddd16a15661
Dion Dirza <dion.dirza@gmail.com>
2020-09-13 21:04:07 +0800

    chore: remove unnecessary babel plugins

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,6 @@ module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
   plugins: [
     'babel-plugin-macros',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-syntax-async-generators',
-    '@babel/plugin-syntax-dynamic-import',
-    ['@babel/plugin-transform-destructuring', { useBuiltIns: true }],
     ['@babel/plugin-transform-runtime', { corejs: 3, helpers: false, regenerator: true }],
   ],
   env: {
