79808b5951742e405a508cd1a4787d5471749fb2
Dion Dirza <dion.dirza@gmail.com>
2020-09-13 11:08:19 +0800

    fix: update import dompurify

--- utils/html/sanitize.js
+++ utils/html/sanitize.js
@@ -1,4 +1,4 @@
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
 export default function sanitize(dirtyHtml, options = null) {
   return DOMPurify.sanitize(dirtyHtml.replace(/\n/g, '<br />'), options);
